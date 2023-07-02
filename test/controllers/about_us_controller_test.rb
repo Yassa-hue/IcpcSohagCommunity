7d730de71a8b53420f8175f3f1c31895daecdacc
yassa <yassatamer99@gmail.com>
2023-07-02 19:48:26 +0300

    correting the about us testing url

--- test/controllers/about_us_controller_test.rb
+++ test/controllers/about_us_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class AboutUsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get about_us_index_url
+    get about_us_url
     assert_response :success
   end
 end
